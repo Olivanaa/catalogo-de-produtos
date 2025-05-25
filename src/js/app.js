74ebc99887276ef2118ce63dd0d5eca6228b0d72
olivanaa <oliv.anaclara@gmail.com>
2025-05-25 00:50:38 -0300

    feat: criação da estrutura, do index e da estilização

---
+++ src/js/app.js
@@ -0,0 +1,74 @@
+const produtos = [
+    { 
+        nome: "Entre o Amor e o Tempo", 
+        preco: 42.90, 
+        categoria: "Romance", 
+        disponibilidade: true 
+    },
+    { 
+        nome: "A Última Fortaleza", 
+        preco: 56.00, 
+        categoria: "Romance",
+        disponibilidade: true 
+    },
+    { 
+        nome: "Cosmos", 
+        preco: 69.90, 
+        categoria: "Suspense", 
+        disponibilidade: true 
+    },
+    { 
+        nome: "Sobre a escrita", 
+        preco: 49.50, 
+        categoria: "Biografia", 
+        disponibilidade: false 
+    },
+    { 
+        nome: "Entendendo Algoritmos", 
+        preco: 59.00, 
+        categoria: "Tecnologia", 
+        disponibilidade: true 
+    },
+    { 
+        nome: "A última testemunha", 
+        preco: 38.70, 
+        categoria: "Suspense", 
+        disponibilidade: true 
+    },
+    { 
+        nome: "Programação para Iniciantes", 
+        preco: 89.90, 
+        categoria: "Tecnologia", 
+        disponibilidade: true 
+    },
+    { 
+        nome: "Ilha do Tesouro", 
+        preco: 32.00, 
+        categoria: "Aventura", 
+        disponibilidade: false 
+    },
+    { 
+        nome: "Aventuras no Ártico", 
+        preco: 44.90, 
+        categoria: "Aventura", 
+        disponibilidade: true 
+    },
+    { 
+        nome: "Mistério na Casa 32", 
+        preco: 36.50, 
+        categoria: "Suspense", 
+        disponibilidade: true 
+    },
+    { 
+        nome: "Lógica de Programação", 
+        preco: 56.50, 
+        categoria: "Tecnologia", 
+        disponibilidade: false 
+    },
+    { 
+        nome: "Feliz Ano Velho", 
+        preco: 74.50, 
+        categoria: "Biografia", 
+        disponibilidade: false 
+    },
+]
